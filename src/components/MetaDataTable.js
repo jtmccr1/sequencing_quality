fa64908f99a1b2693d16a81600ab47e48c2c0e20
JT <mccrone@umich.edu>
2018-08-29 11:28:20 +0100

    table formatting

--- src/components/MetaDataTable.js
+++ src/components/MetaDataTable.js
@@ -19,38 +19,56 @@ const selectRowProp = {
 const textColumn = {
 	dataAlign: 'left',
 	headerAlign: 'left',
+	filter: { type: 'TextFilter', delay: 500 },
 };
 const numberColumn = {
 	dataAlign: 'center',
 	headerAlign: 'center',
 };
+
 class MetaDataTable extends Component {
+	constructor(props) {
+		super(props);
+
+		this.options = {
+			defaultSortName: 'ENROLLID', // default sort column name
+			defaultSortOrder: 'desc', // default sort order
+		};
+	}
 	render() {
 		return (
 			<div>
-				<BootstrapTable data={this.props.data} selectRow={selectRowProp}>
-					<TableHeaderColumn isKey dataField="SPECID" {...textColumn}>
+				<h2>Samples to analyze</h2>
+				<BootstrapTable
+					data={this.props.data}
+					striped
+					hover
+					condensed
+					selectRow={selectRowProp}
+					options={this.options}
+				>
+					<TableHeaderColumn isKey dataField="SPECID" dataSort width="100" {...textColumn}>
 						Specimen
 					</TableHeaderColumn>
-					<TableHeaderColumn dataField="ENROLLID" {...textColumn}>
+					<TableHeaderColumn dataField="ENROLLID" dataSort width="80" {...textColumn}>
 						Enrollee
 					</TableHeaderColumn>
-					<TableHeaderColumn dataField="HOUSE_ID" {...textColumn}>
+					<TableHeaderColumn dataField="HOUSE_ID" dataSort width="80" {...textColumn}>
 						House
 					</TableHeaderColumn>
-					<TableHeaderColumn dataField="onset" {...numberColumn}>
+					<TableHeaderColumn dataField="onset" dataSort width="120" {...numberColumn}>
 						Date of Symptom Onset
 					</TableHeaderColumn>
-					<TableHeaderColumn dataField="collect" {...numberColumn}>
+					<TableHeaderColumn dataField="collect" dataSort width="120" {...numberColumn}>
 						Date of Collection
 					</TableHeaderColumn>
-					<TableHeaderColumn dataField="DPI" {...numberColumn}>
+					<TableHeaderColumn dataField="DPI" dataSort width="120" {...numberColumn}>
 						Days post onset
 					</TableHeaderColumn>
-					<TableHeaderColumn dataField="gc_ul" {...numberColumn}>
+					<TableHeaderColumn dataField="gc_ul" dataSort width="120" {...numberColumn}>
 						Titer (gc/ul)
 					</TableHeaderColumn>
-					<TableHeaderColumn dataField="vaccination_status" {...textColumn}>
+					<TableHeaderColumn dataField="vaccination_status" dataSort width="150" {...numberColumn}>
 						Vaccination Status
 					</TableHeaderColumn>
 				</BootstrapTable>
