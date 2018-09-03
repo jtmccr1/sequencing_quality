26b702fc13bafdf455ee015f62e18fd81409db3b
JT <mccrone@umich.edu>
2018-09-03 16:59:53 +0100

    starting to control the table from above

--- src/components/MetaDataTable.js
+++ src/components/MetaDataTable.js
@@ -15,27 +15,44 @@ const numberColumn = {
 class MetaDataTable extends Component {
 	constructor(props) {
 		super(props);
-		this.onSelectRow = this.onSelectRow.bind(this);
-		this.options = {
-			defaultSortName: 'ENROLLID', // default sort column name
-			defaultSortOrder: 'desc', // default sort order
-		};
-		this.selectRowProp = {
-			mode: 'checkbox',
-			clickToSelect: true,
-			unselectable: [2],
-			selected: [1],
-			onSelect: this.onSelectRow,
-			bgColor: 'gold',
+		this.onSelectAll = this.onSelectAll.bind(this);
+		this.onRowSelect = this.onRowSelect.bind(this);
+		this.state = {
+			selected: [],
 		};
 	}
-	onSelectRow(row, isSelected, e) {
+	onRowSelect({ SPECID }, isSelected) {
 		if (isSelected) {
-			this.props.selectorFunction(row);
+			this.setState({
+				selected: [...this.state.selected, SPECID],
+			});
+		} else {
+			this.setState({ selected: this.state.selected.filter(it => it !== SPECID) });
 		}
+		return false;
+	}
+
+	onSelectAll(isSelected) {
+		if (!isSelected) {
+			this.setState({ selected: [] });
+		}
+		return false;
 	}
 
 	render() {
+		const selectRowProp = {
+			mode: 'checkbox',
+			clickToSelect: true,
+			onSelect: this.onRowSelect,
+			onSelectAll: this.onSelectAll,
+			selected: this.state.selected,
+		};
+
+		const options = {
+			sortName: 'ENROLLID',
+			sortOrder: 'desc',
+		};
+
 		return (
 			<div>
 				<h2>Samples to analyze</h2>
@@ -44,8 +61,8 @@ class MetaDataTable extends Component {
 					striped
 					hover
 					condensed
-					selectRow={this.selectRowProp}
-					options={this.options}
+					selectRow={selectRowProp}
+					options={options}
 				>
 					<TableHeaderColumn isKey dataField="SPECID" dataSort width="100" {...textColumn}>
 						Specimen
